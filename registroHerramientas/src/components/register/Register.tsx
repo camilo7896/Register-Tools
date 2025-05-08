1bf7d97fa0dd050c11b3446bbff4453b23e238c8
Cristian Camilo Garcia Gutierrez <cg082490@gmail.com>
2025-05-07 20:58:42 -0500

    se modifico la información de la card

--- registroHerramientas/src/components/register/Register.tsx
+++ registroHerramientas/src/components/register/Register.tsx
@@ -87,7 +87,7 @@ if (loading) {
                                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{tool.tool}</h5>
                             </a>
                             <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.
+                            <strong>{tool.responsible}</strong> indica que solicita la salida de <strong>{tool.tool}</strong> por motivo de <strong>{tool.reason}</strong> y que sera devuelto el <strong>{tool.returnDate}</strong> 
                             </p>
                             <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                                 Ver mas
