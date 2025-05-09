3ef2a9c9b17c1e5dfb6ff4f0459212291dd8b124
Cristian Camilo Garcia Gutierrez <cg082490@gmail.com>
2025-05-09 15:05:31 -0500

    Se cambio el codigo del li de condional de administrador

--- registroHerramientas/src/components/Navbar.tsx
+++ registroHerramientas/src/components/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: React.FC<userProps> = ({ Userloged }) => {
     const menuRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
-            // Cierra el menú si se hace clic fuera
+        // Cierra el menú si se hace clic fuera
         const handleClickOutside = (event: MouseEvent) => {
             if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
                 setMenuOpen(false);
@@ -50,17 +50,17 @@ const Navbar: React.FC<userProps> = ({ Userloged }) => {
             });
     };
 
-const fetchUserRole = async () => {
-            const user = auth.currentUser;
-            if (user) {
-                const userDoc = await getDocs(query(collection(db, "users")));
-                const userData = userDoc.docs.find(docu => docu.data().email === user.email);
-                if (userData) {
-                    setUserRole(userData.data().role || null);
-                }
+    const fetchUserRole = async () => {
+        const user = auth.currentUser;
+        if (user) {
+            const userDoc = await getDocs(query(collection(db, "users")));
+            const userData = userDoc.docs.find(docu => docu.data().email === user.email);
+            if (userData) {
+                setUserRole(userData.data().role || null);
             }
-        };
-        fetchUserRole();
+        }
+    };
+    fetchUserRole();
 
     return (
         <>
@@ -79,10 +79,10 @@ const fetchUserRole = async () => {
                         >
                             <div className="flex flex-col items-center">
 
-                            <div className=" relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                                <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
-                            </div>
-                                    <small className="text-white mt-2">Abrir menu</small>
+                                <div className=" relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+                                    <svg className="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+                                </div>
+                                <small className="text-white mt-2">Abrir menu</small>
                             </div>
 
                         </button>
@@ -105,11 +105,17 @@ const fetchUserRole = async () => {
                                 <li>
                                     <Link to="/register" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Registro</Link>
                                 </li>
-                                {userRole !== "superadmin"?"":
-                                <li>
-                                    <Link to="/admin" className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Administrador</Link>
-                                </li>
-                                }
+                                {userRole === "superadmin" && (
+                                    <li>
+                                        <Link
+                                            to="/admin"
+                                            className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                        >
+                                            Administrador
+                                        </Link>
+                                    </li>
+                                )}
+
                             </ul>
                             <button onClick={handleLogout} className="bg-gray-600 text-white text-center block w-full font-semibold px-4 py-2 text-sm hover:bg-gray-500 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
                                 Cerrar sesión
@@ -124,11 +130,17 @@ const fetchUserRole = async () => {
                             <li>
                                 <Link to="/register" className="items block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Registro</Link>
                             </li>
-                             {userRole !== "superadmin"?"":
+                            {userRole === "superadmin" && (
                                 <li>
-                                    <Link to="/admin" className="text-white block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Administrador</Link>
+                                    <Link
+                                        to="/admin"
+                                        className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                                    >
+                                        Administrador
+                                    </Link>
                                 </li>
-                                }
+                            )}
+
                         </ul>
                     </div>
                 </div>
